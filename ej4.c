b33776afbf81cf2d6ccbfb6380d3783333ffe516
ashandme <santiagosoleto121@gmail.com>
2021-11-24 11:36:29 -0300

    4

--- ej4.c
+++ ej4.c
@@ -5,7 +5,7 @@ void mostrar(int r, int c, int g[r][c]){
     for(int i = 0; i < c; i++){
         printf("%d:\t", i+1);
         for(int f = 0; f < r; f++){
-            printf("%d\t", g[r][c]);
+            printf("%d\t", g[f][i]);
         }
         putchar('\n');
     }
@@ -14,25 +14,21 @@ int main(){
   int t = 0, tl = 0;
   int a[3][COL];
   for(int i = 0; i < 3; i++){
-      for (int f = 0; i < COL; i++){
+      for (int f = 0; f < COL; f++){
           a[i][f] = 0;
       }
   }
+  int j, k, l;
   printf("ingrese el número de linea, el número de coche y su recaudación\n");
-  for(;;){//No conozco la cantidad de veces que se repite ni el orden de ingreso.
-          int j, k, l;
+  char y[3];
+  while(y[0] != 's'){//No conozco la cantidad de veces que se repite ni el orden de ingreso.
           scanf("%d %d %d", &j, &k, &l);
           if((j > 3 || j < 1) || (k > COL || k < 1))
               continue;
-          a[j-1][k-1] = l;
+          a[j-1][k-1] += l;
           mostrar(3, COL, a);
-          printf("desea ingresar otro? ");
-          char y;
-          scanf("%c", &y);
-          if (y == 's')
-              continue;
-          else
-              break;
+          printf("desea terminar con el ingreso? ");
+          scanf("%s", y);
   }
   for(int i = 0; i < 3; i++){
     tl = 0;
